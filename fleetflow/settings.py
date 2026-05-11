60deb23ad6ff7e50120d4d1b594381f36bcf1ad3
Maxwell Githinji <muthondugithinji@gmail.com>
2026-05-11 14:22:33 +0300

    Add allowed host for fleetflow backend
    Add allowed host for fleetflow backend

--- fleetflow/settings.py
+++ fleetflow/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', 'fleetflow-backend-zr0z.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
